1c589863d33c64f955ae50cf5b288af1b08ce7f4
chenjianxing <jianxing.chen@fit2cloud.com>
2021-08-10 15:52:58 +0800

    refactor: 测试用例关联测试重构

--- backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
+++ backend/src/main/java/io/metersphere/api/service/ApiAutomationService.java
@@ -2460,4 +2460,13 @@ public class ApiAutomationService {
         result.setCheckMsg(checkMsgList);
         return result;
     }
+
+    public List<ApiScenario> getScenarioCaseByIds(List<String> ids) {
+        if (CollectionUtils.isNotEmpty(ids)) {
+            ApiScenarioExample example = new ApiScenarioExample();
+            example.createCriteria().andIdIn(ids);
+            return apiScenarioMapper.selectByExample(example);
+        }
+        return new ArrayList<>();
+    }
 }
